553c289695b1f5db3858e8ade4b20901f7d98d3c
Alireza <arwix79@gmail.com>
2022-09-08 01:29:39 +0430

    Create polls app and views

--- MyProject/settings.py
+++ MyProject/settings.py
@@ -21,11 +21,8 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/3.2/howto/deployment/checklist/
 
-# SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
 
-ALLOWED_HOSTS = []
-
+ALLOWED_HOSTS = ["*"]
 
 # Application definition
 
