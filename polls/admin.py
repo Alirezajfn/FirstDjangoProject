3f01b5b817ef08ba3b1a0b85ae1a136963a918a7
Alireza <arwix79@gmail.com>
2022-09-10 23:04:17 +0430

    Creating models and database

--- polls/admin.py
+++ polls/admin.py
@@ -1,3 +1,16 @@
 from django.contrib import admin
+from polls.models import Question, Choice
 
-# Register your models here.
+
+class ChoiceInline(admin.TabularInline):
+    model = Choice
+    extra = 3
+
+
+class QuestionAdmin(admin.ModelAdmin):
+    fields = [(None, {'fields': ['question_text']}),
+              ('Date information', {'fields': ['pub_date'], 'classes': ['collapse']}),
+              ]
+
+
+admin.site.register(Question, QuestionAdmin)
